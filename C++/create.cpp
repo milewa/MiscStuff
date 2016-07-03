35e8e5cec1d54950387404cab3744cb593d96f3d
Michael Wang <Michael Wang>
2016-07-02 22:28:41 -0600

    Started create.cpp

--- C++/create.cpp
+++ C++/create.cpp
@@ -1,9 +1,38 @@
 #include<iostream>
+#include<fstream>
+#include<vector>
+#include<string>
 
 using namespace std;
 
-int main(){
-	
+void printIncludes;
+void printMain(stringstream, bool);
+
+int main(int argc, char* argv[]){
+	ofstream newCppFile;
+	vector<string> includes;
+	string filename;
+
+	filename(argv[argc - 1]);
+	newCppFile.open(filename);
+
+	printMain(filename, true);
+
+	for (int i = 1; i < argc; i++) {
+					
+	}
 
     return 0;
 }
+
+void printMain(ofstream& of, bool cmdArgs) {
+	if(cmdArgs){
+		of << "int main(int argc, char* argv[]){\n";
+	}else{
+		of << "int main(){\n";
+	}
+
+	of << "\nreturn 0;\n}";
+}
+
+void printIncludes(){}
